94c17bb8f680deb0d910a82eedee913c217e4d4d
Omilois101 <79018342+Omilois101@users.noreply.github.com>
2022-10-17 15:43:00 +0100

    Update dataswapper.vhd

--- dataswapper.vhd
+++ dataswapper.vhd
@@ -15,8 +15,16 @@ port(
 end data_swapper;
 
 architecture data_swapper_arch of data_swapper is 
+Type data_list is array (0 to 3) of std_logic_vector ( 7 downto 0); 
 
 begin 
-  
-  
+  process(dmao)
+     variable data_list_original: data_list; 
+     variable data_list_swapped: data_list;
+     begin
+          data_list_original(0):= dmao.rdata(7 downto 0)
+          data_list_original(1):= dmao.rdata(15 downto 8)
+          data_list_original(2):= dmao.rdata(23 downto 16)
+          data_list_original(3):= dmao.rdata(31 downto 24)
+     end 
 end data_swapper_arch; 
